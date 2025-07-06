38bd74b9f1760173a89db3c0e901c6edc7e29bb3
ubergeek <ubergeek@UberMacBook.local>
2025-07-06 15:29:25 -0700

    Fix year column migration with proper type conversion

--- priv/repo/migrations/20250706221903_fix_year_column_type.exs
+++ priv/repo/migrations/20250706221903_fix_year_column_type.exs
@@ -2,9 +2,6 @@ defmodule SimpleApp.Repo.Migrations.FixYearColumnType do
   use Ecto.Migration
 
   def change do
-    execute "UPDATE bikes SET year = NULL WHERE year = ''"
-    alter table(:bikes) do
-      modify :year, :integer, using: "year::integer"
-    end
+    execute "ALTER TABLE bikes ALTER COLUMN year TYPE integer USING CASE WHEN year ~ '^[0-9]+$' THEN year::integer ELSE NULL END"
   end
 end
