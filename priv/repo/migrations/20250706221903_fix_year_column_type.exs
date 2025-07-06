dad06236f9aa8eaab7b201b81e7c53a06a06818c
ubergeek <ubergeek@UberMacBook.local>
2025-07-06 15:22:07 -0700

    Fix year column migration to handle empty strings

--- priv/repo/migrations/20250706221903_fix_year_column_type.exs
+++ priv/repo/migrations/20250706221903_fix_year_column_type.exs
@@ -2,8 +2,9 @@ defmodule SimpleApp.Repo.Migrations.FixYearColumnType do
   use Ecto.Migration
 
   def change do
+    execute "UPDATE bikes SET year = NULL WHERE year = ''"
     alter table(:bikes) do
-      modify :year, :integer, using: "CASE WHEN year = '' THEN NULL ELSE year::integer END"
+      modify :year, :integer, using: "year::integer"
     end
   end
 end
